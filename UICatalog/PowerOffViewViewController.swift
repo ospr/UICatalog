6a84095414fa9c454ee53c1a119db59939cccf27
Kip Nicol <ospr@me.com>
2016-08-07 15:16:34 -0700

    Update support for creating PowerOff vc.

--- UICatalog/PowerOffViewViewController.swift
+++ UICatalog/PowerOffViewViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
+// TODO: fix name here
 class PowerOffViewViewController: UIViewController {
-
+    
+    required init() {
+        super.init(nibName: String(PowerOffViewViewController.self), bundle: nil)
+        
+        title = "Power Off"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented. Use init()")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 }
