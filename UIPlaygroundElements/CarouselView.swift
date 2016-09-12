c6053a18d0a0a061a8094172a90655d567bd58df
Kip Nicol <ospr@me.com>
2016-09-12 12:08:56 -0700

    Not much gained from this todo and will require more code.

--- UIPlaygroundElements/CarouselView.swift
+++ UIPlaygroundElements/CarouselView.swift
@@ -147,7 +147,6 @@ public class CarouselView: UIView, UIGestureRecognizerDelegate {
     // MARK: - Working with Data
     
     public func reloadData() {
-        // TODO: could put this all in a struct and have it all reset properly each time rather than trying to maintain the state here
         for itemView in itemViews {
             itemView.removeFromSuperview()
         }
