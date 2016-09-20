d97daa8fdb8a502419307b77ae48148f867977d8
Kip Nicol <ospr@me.com>
2016-09-20 09:02:21 -0700

    Add spring board view.

---
+++ UIPlayground/SpringBoardViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SpringBoardViewController.swift
+//  UIPlayground
+//
+//  Created by Kip Nicol on 9/20/16.
+//  Copyright © 2016 Kip Nicol. All rights reserved.
+//
+
+import UIKit
+
+class SpringBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
